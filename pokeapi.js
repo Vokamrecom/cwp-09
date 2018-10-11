6352bc5607b07c3ad4a72db2887af1181a8e9b7d
Kiril <ermakovkiril@gmail.com>
2018-10-11 18:42:20 +0300

    task 1.2

--- pokeapi.js
+++ pokeapi.js
@@ -2,7 +2,7 @@ const Promise = require('bluebird');
 const axios = require('axios');
 
 /*      Task 1.1      */
-
+/*
 axios.get('http://pokeapi.co/api/v2/pokemon/42')
     .then((response) => {
         console.log(`    id: ${response.data.id}
@@ -13,3 +13,30 @@ axios.get('http://pokeapi.co/api/v2/pokemon/42')
     .catch((error) => {
         console.log(error);
     });
+    */
+
+/*      Task 1.2      */
+
+let poke_array = []
+for (var i = 0; i < 2; ++i) {
+  poke_array.push(
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${i}0`)
+    .then((response) => {
+      return response.data.results;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  );
+}
+Promise.all(poke_array)
+  .then((response) => {
+    response.forEach((arr, counter) => {
+        arr.forEach((pokemon, pok_number) => {
+            console.log(`${counter + 1}.${pok_number + 1}: ${pokemon.name}`);
+        });
+    });
+  })
+  .catch((error) => {
+      console.log(error);
+});
