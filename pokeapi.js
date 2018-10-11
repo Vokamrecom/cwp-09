ee555d3c4ef8d8d965b7f9e9b574cf3daa8ff214
Kiril <ermakovkiril@gmail.com>
2018-10-11 18:31:56 +0300

    task 1.1

--- pokeapi.js
+++ pokeapi.js
@@ -1,2 +1,15 @@
 const Promise = require('bluebird');
 const axios = require('axios');
+
+/*      Task 1.1      */
+
+axios.get('http://pokeapi.co/api/v2/pokemon/42')
+    .then((response) => {
+        console.log(`    id: ${response.data.id}
+    Name: ${response.data.name}
+    Weight: ${response.data.weight}
+    Height: ${response.data.height}`);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
